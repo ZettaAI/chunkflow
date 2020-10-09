ef75cc443bceb09da51e590e830bae0146c563f6
Ran Lu <ranl@zetta.ai>
2020-10-08 21:46:26 -0400

    Try harder to drain the tasks from the queue

--- scripts/drain_tasks.sh
+++ scripts/drain_tasks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 chunkflow \
-    fetch-task-kombu -r 1 --queue-name=amqp://172.31.31.249:5672 \
+    fetch-task-kombu -r 5 --queue-name=amqp://172.31.31.249:5672 \
     delete-task-in-queue-kombu
 
