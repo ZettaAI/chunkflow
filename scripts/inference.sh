2a478970541c26aed4902a6f211135270b1ee90d
Ran Lu <ranl@zetta.ai>
2021-03-07 13:39:18 -0500

    Try harder
    The heartbeat timeout is 600s, a worker should wait longer than that
    to make sure it catches invisible messages

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -26,7 +26,7 @@ echo ${CONTRAST_NORMALIZATION}
 echo ${POST_PROCESS}
 
 chunkflow --mip ${OUTPUT_MIP} --verbose 0 \
-    fetch-task-kombu -r 5 --queue-name=amqp://172.31.31.249:5672 \
+    fetch-task-kombu -r 10 --queue-name=amqp://172.31.31.249:5672 \
     cutout --mip ${IMAGE_MIP} --volume-path="$IMAGE_PATH" --expand-margin-size ${EXPAND_MARGIN_SIZE} ${IMAGE_FILL_MISSING} \
     ${CONTRAST_NORMALIZATION} \
     ${MASK_IMAGE} \
